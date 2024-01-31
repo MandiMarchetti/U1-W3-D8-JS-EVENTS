3c2083d24545c59e6b6a4bd3e033f4280c24e120
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-01-31 17:58:32 +0100

    update

---
+++ index.js
@@ -0,0 +1,30 @@
+const submitTheAnswer = function(){
+    const bottone = document.getElementsByClassName("box-button");
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+const handButtonToCleanForm = function (e) {
+    console.log("Bottone cliccato!");
+    //console.log("questo è l'event object della funzione handleButtonClickFromAttribute", e);
+
+    //btn.style.backgroundColor = "red";
+    //console.log("testo del bottone", e.target.innerText);
+        
+    const btn = e.target;
+    console.log("testo del bottone", e.target.innerText);
+};
+
+
+
+
